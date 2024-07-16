2fd3bb5f119d79b9b7ed8cb4fe53b45fd396fea0
Enderson Namias <enderson.namias@tealca.com>
2024-07-16 18:31:03 -0400

    NotFound

---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/display-name */
+
+import Link from 'next/link';
+
+/* eslint-disable import/no-anonymous-default-export */
+export default function () {
+  return (
+    <div>
+      <h1>404 N ot Found</h1>
+      <Link href="/">Regresar</Link>
+    </div>
+  );
+}
--- src/components/ui/topmenu/TopMenu.tsx
+++ src/components/ui/topmenu/TopMenu.tsx
@@ -1,9 +1,10 @@
 import { titleFont } from '@/config/fonts';
 import Link from 'next/link';
+import { IoCartOutline, IoSearchOutline } from 'react-icons/io5';
 
 export const TopMenu = () => {
   return (
-    <nav className="flex px-5 justify-between items-center w-full">
+    <nav className="flex px-5 justify-between items-center w-full pt-2">
       {/*Logo*/}
       <div>
         <Link href="/">
@@ -14,7 +15,7 @@ export const TopMenu = () => {
       </div>
 
       {/* CenterMenu */}
-      <div className="">
+      <div className="hidden sm:block">
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
           href={'/category/men'}
@@ -33,6 +34,24 @@ export const TopMenu = () => {
         >
           Niños
         </Link>
+      </div>
+
+      {/* search, Cart, Menu */}
+      <div className="flex items-center">
+        <Link href={'/search'} className="mx-2">
+          <IoSearchOutline className="w-5 h-5"></IoSearchOutline>
+        </Link>
+        <Link href={'/cart'} className="mx-2">
+          <div className="relative">
+            <span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5"></IoCartOutline>
+          </div>
+        </Link>
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          Menú
+        </button>
       </div>
     </nav>
   );
